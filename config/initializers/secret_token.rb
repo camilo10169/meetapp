83b79850c7b07dd3160d5e21a464ceb5497425fa
Mariano Aguirre Giraldo <marianoaguirregiraldo@gmail.com>
2013-08-31 18:59:19 -0500

    MeetApp Project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Meetapp::Application.config.secret_key_base = '21559b4f65e4d0710120331725c2f4308278b46301a74dbe18305e9df436ded8f1339858b871b5767aa7c19042333049db8f8845c77b2ae470bf44560b53514e'
